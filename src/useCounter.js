16b166a6393f9adfad5beb23b106c67a7de2b1bf
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-11-25 23:07:07 +0900

    React + Unstated Next: 複数コンポーネントのツリーの中で状態を共有して管理する

---
+++ src/useCounter.js
@@ -0,0 +1,11 @@
+import { useCallback, useState } from "react";
+import { createContainer } from "unstated-next";
+
+const useCounter = (initialState = 0) => {
+	const [count, setCount] = useState(initialState);
+	const decrement = useCallback(() => setCount((prevCount) => prevCount - 1), []);
+	const increment = useCallback(() => setCount((prevCount) => prevCount + 1), []);
+	return { count, decrement, increment };
+};
+
+export const CounterContainer = createContainer(useCounter);
